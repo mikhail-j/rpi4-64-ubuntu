65618888eacf3c65717d807a0060fd82b444fddf
mikhail-j <mikhail.jin@gmail.com>
2019-11-01 18:53:42 -0400

    Added shell scripts to compile the Linux kernel for Ubuntu 19.10 bootable Raspberry Pi 4 image.

---
+++ 19.10/build_ubuntu_arm64_image.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+# Generate a 64-bit Ubuntu Server 19.10 (Eoan Ermine) image for the Raspberry Pi 4.
+#
+# Note: This script was written to modify the Ubuntu Server preinstalled cloud
+# image for the Raspberry Pi 3 on Ubuntu.
+#
+# Copyright (C) 2019 Qijia (Michael) Jin
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# check if arm64 toolchain directory exists
+ARM64_TOOLCHAIN=$(pwd)/rpi64/arm64_toolchain/aarch64
+if !(test -d $ARM64_TOOLCHAIN); then
+	echo "error: could not find arm64 toolchain!"
+	exit 1
+fi
+
+if !(test -d $(pwd)/rpi64/linux/build); then
+	echo "error: could not find expected arm64 linux kernel!"
+	exit 1
+fi
+
+# check if Raspberry Pi 4 `config.txt` exists in current directory
+if !(test -e "config.txt"); then
+	echo "error: expected 'config.txt' in current directory!"
+	exit 1
+fi
+
+# check if raspberrypi/firmware repository has been cloned
+if !(test -d $(pwd)/rpi64/firmware/); then
+	cd rpi64
+	git clone https://github.com/raspberrypi/firmware/
+	if test $? -ne 0; then
+		# remove git repository 'raspberrypi/firmware' and exit
+		rm -rf firmware
+		exit 1
+	fi
+
+	cd ../
+fi
+
+# make sure we have kpartx on Ubuntu
+sudo apt-get install kpartx
+if test $? -ne 0; then
+	exit $?
+fi
+
+LATEST_EOAN=$(curl http://cdimage.ubuntu.com/ubuntu/releases/eoan/release/ | grep "arm64+raspi3.img.xz\"" | head -n 1 | cut -d '"' -f 2)
+
+if !(test -e $LATEST_EOAN); then
+	wget http://cdimage.ubuntu.com/ubuntu/releases/eoan/release/$LATEST_EOAN
+	if test $? -ne 0; then
+		exit $?
+	fi
+	curl http://cdimage.ubuntu.com/ubuntu/releases/eoan/release/SHA256SUMS | grep arm64+raspi3 > $LATEST_EOAN.sha256
+	if test $? -ne 0; then
+		exit $?
+	fi
+fi
+
+sha256sum -c "$LATEST_EOAN".sha256
+if test $? -ne 0; then
+	exit $?
+fi
+
+MODIFIED_IMAGE=$(echo "$LATEST_EOAN" | cut -d '+' -f 1)+rpi4.img
+
+xzcat $LATEST_EOAN > $MODIFIED_IMAGE
+if test $? -ne 0; then
+	exit $?
+fi
+
+if !(test -d root); then
+	mkdir root
+	if test $? -ne 0; then
+		exit $?
+	fi
+fi
+
+if !(test -d boot); then
+	mkdir boot
+	if test $? -ne 0; then
+		exit $?
+	fi
+fi
+
+LOOP_DEVICE=$(sudo kpartx -av $MODIFIED_IMAGE | head -n 1 | awk ' { print $3 } ' | cut -d 'p' -f 1,2)
+
+echo "$LOOP_DEVICE" | grep "loop" && echo "loop device mapped!"
+if test $? -ne 0; then
+	exit $?
+fi
+echo "loop device: $LOOP_DEVICE"
+
+KERNEL_VERSION=$(cat rpi64/linux/build/include/generated/utsrelease.h | awk ' { gsub(/"/,""); print $3 } ')
+
+sudo mount /dev/mapper/"$LOOP_DEVICE"p2 root
+sudo mount /dev/mapper/"$LOOP_DEVICE"p1 boot
+
+sudo cp -avf $(pwd)/rpi64/linux/build/install/lib/modules/$KERNEL_VERSION root/lib/modules/
+
+sudo cp $(pwd)/rpi64/linux/build/arch/arm64/boot/Image $(pwd)/boot/kernel8.img
+sudo cp $(pwd)/rpi64/firmware/boot/*.dat $(pwd)/boot/
+sudo cp $(pwd)/rpi64/firmware/boot/*.elf $(pwd)/boot/
+sudo cp $(pwd)/rpi64/firmware/boot/bootcode.bin $(pwd)/boot/
+sudo cp $(pwd)/rpi64/linux/build/arch/arm64/boot/dts/broadcom/*.dtb $(pwd)/boot/
+
+sudo rm -rf $(pwd)/boot/overlays/
+sudo cp -R $(pwd)/rpi64/linux/build/arch/arm64/boot/dts/overlays/ $(pwd)/boot/
+
+sudo cp -R $(pwd)/config.txt $(pwd)/boot/
+
+
+# sleep for 10 seconds to allow for unmounting, otherwise loop device is busy
+sleep 10
+
+sudo umount root
+sudo umount boot
+
+sudo kpartx -dv $MODIFIED_IMAGE
+
+
+
+
---
+++ 19.10/build_ubuntu_arm64_kernel.sh
@@ -0,0 +1,152 @@
+#!/bin/sh
+# Build ARM64 toolchain and compile 'github.com/raspberrypi/linux'
+# git repository's branch (rpi-4.19.y) of the Linux Kernel.
+#
+# Note: This script was written to build the Linux kernel on Ubuntu.
+#
+# Copyright (C) 2019 Qijia (Michael) Jin
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# calculate the number of CPU threads
+THREAD_COUNT=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+# make sure we have the required header files on Ubuntu
+sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev
+
+# make sure we have the required packages to compile the linux kernel
+sudo apt-get install bison flex
+
+# check if arm64 toolchain directory exists
+ARM64_TOOLCHAIN=$(pwd)/rpi64/arm64_toolchain/aarch64
+if !(test -d $ARM64_TOOLCHAIN); then
+	mkdir -p rpi64/arm64_toolchain/aarch64
+fi
+
+# change directory to 'rpi64/arm64_toolchain/'
+cd rpi64/arm64_toolchain/
+if test $? -ne 0; then
+	exit $?
+fi
+
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.bz2
+if test $? -ne 0; then
+	exit $?
+fi
+
+# change directory to 'rpi64/arm64_toolchain/aarch64/'
+cd $ARM64_TOOLCHAIN
+tar -xjf ../binutils-2.32.tar.bz2
+if test $? -ne 0; then
+	exit $?
+fi
+
+cd binutils-2.32
+if test $? -ne 0; then
+	exit $?
+fi
+./configure --prefix=$ARM64_TOOLCHAIN --target=aarch64-linux-gnu
+if test $? -ne 0; then
+	exit $?
+fi
+make -j $THREAD_COUNT
+if test $? -ne 0; then
+	exit $?
+fi
+make check
+if test $? -ne 0; then
+	exit $?
+fi
+make install
+if test $? -ne 0; then
+	exit $?
+fi
+
+# change directory to 'rpi64/arm64_toolchain/'
+cd ../../
+
+wget https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
+if test $? -ne 0; then
+	exit $?
+fi
+
+cd aarch64
+
+tar -xJf ../gcc-9.2.0.tar.xz
+if test $? -ne 0; then
+	exit $?
+fi
+
+cd gcc-9.2.0
+if test $? -ne 0; then
+	exit $?
+fi
+./configure --prefix=$ARM64_TOOLCHAIN --target=aarch64-linux-gnu --with-newlib --without-headers \
+--disable-shared --disable-threads --disable-libssp --disable-decimal-float --disable-libquadmath \
+--disable-libvtv --disable-libgomp --disable-libatomic --enable-languages=c,c++,fortran
+if test $? -ne 0; then
+	exit $?
+fi
+make all-gcc -j $THREAD_COUNT
+if test $? -ne 0; then
+	exit $?
+fi
+
+make -j $THREAD_COUNT check
+# `make check` is expected to fail for GCC
+# This is due to the `dejagnu` dependency for `runtest`.
+
+make install-gcc
+if test $? -ne 0; then
+	exit $?
+fi
+
+# change directory to 'rpi64'
+cd ../../../
+
+# clone 'raspberrypi/linux' git repository and compile the 'rpi-4.19.y' branch of the Linux kernel
+git clone https://github.com/raspberrypi/linux.git
+if test $? -ne 0; then
+	exit $?
+fi
+cd linux
+if test $? -ne 0; then
+	exit $?
+fi
+git checkout rpi-4.19.y
+if test $? -ne 0; then
+	exit $?
+fi
+
+mkdir build
+if test $? -ne 0; then
+	exit $?
+fi
+
+env PATH=$ARM64_TOOLCHAIN/bin:$PATH make O=build ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+if test $? -ne 0; then
+	exit $?
+fi
+env PATH=$ARM64_TOOLCHAIN/bin:$PATH make -j $THREAD_COUNT O=build ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+if test $? -ne 0; then
+	exit $?
+fi
+
+KERNEL_VERSION=$(cat build/include/generated/utsrelease.h | awk ' { gsub(/"/,""); print $3 } ')
+
+sudo make -j $THREAD_COUNT O=build DEPMOD=echo MODLIB=install/lib/modules/${KERNEL_VERSION} INSTALL_FW_PATH=install/lib/firmware modules_install
+if test $? -ne 0; then
+	exit $?
+fi
